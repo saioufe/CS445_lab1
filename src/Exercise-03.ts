d7f189bf84a026d26fbfc28d56928a56818cce35
saioufe <saioufeyoutube@yahoo.com>
2022-11-04 00:34:35 -0500

    lab2

---
+++ src/Exercise-03.ts
@@ -0,0 +1,20 @@
+class Car {
+    name :string;
+    acceleration :number;
+    constructor(name:string) {
+        this.name = name;
+        this.acceleration = 0;
+    }
+    honk() {
+        console.log(` ${this.name} is saying: Toooooooooot!`);
+    }
+    accelerate(speed :number) {
+        this.acceleration = this.acceleration + speed;
+    }
+}
+
+let car = new Car("BMW");
+car.honk(); // BMW is saying: Toooooooooot!
+console.log(car.acceleration); // 0
+car.accelerate(60);
+console.log(car.acceleration); // 60
